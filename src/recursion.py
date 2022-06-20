f7ed8159a07e90d4cf3e9ced32ec2a488a5e3d09
ONEASH <oneash082498@gmail.com>
2022-06-21 00:00:50 +0900

    Modified Recursion

--- src/recursion.py
+++ src/recursion.py
@@ -6,4 +6,13 @@ def factorial(n):
         return n * factorial(n - 1)
 
 print(factorial(3))
-print(factorial(-1))
+
+
+def fibonacci(n):
+    assert n >= 0 and int(n) == n, "Fibonacci number must be positive integer only"
+    if n in [0, 1]:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(6))
