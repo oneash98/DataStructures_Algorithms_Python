425480ff6386c4cd26c0eeefd7250747ff14d3a0
ONEASH <oneash082498@gmail.com>
2022-07-08 00:07:02 +0900

    Modified Array

---
+++ src/Array/createArray.py
@@ -0,0 +1,28 @@
+from array import *
+
+arr1 = array('i', [1, 2, 3, 4, 5, 6])
+arr2 = array('d', [1.3, 1.5, 1.6])
+
+arr1.insert(6, 7)
+
+
+
+def traverseArray(array):
+    for i in array:
+        print(i)
+
+
+def accessElement(array, index):
+    if index > len(array):
+        print('error')
+    else:
+        print(array[index])
+
+def searchInArray(array, value):
+    for i in array:
+        if i == value:
+            return array.index(value)
+    return "The element does not exist"
+
+print(searchInArray(arr1, 3))
+print(arr1.index(8))
